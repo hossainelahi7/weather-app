af893cf705ce65b3c3ebd0c6217860bf3fac24ee
Hossain Elahi <hossain.elahi@live.com>
2021-04-11 15:20:36 -0400

    created material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatCardModule} from '@angular/material/card'
+import {MatButtonModule} from '@angular/material/button'
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { LocationComponent } from './location/location.component';
@@ -14,7 +18,12 @@ import { LocationService } from './location.service';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatCardModule, 
+    MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [
     LocationService
